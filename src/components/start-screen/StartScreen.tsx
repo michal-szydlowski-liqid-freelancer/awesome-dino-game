b8a38b4bc25cf9d2fdb66692e4a666efc0feeb6a
Ingrid Amaral <ingrid.amaral@liqid.de>
2021-12-08 18:17:08 +0100

    button and screen

---
+++ src/components/start-screen/StartScreen.tsx
@@ -0,0 +1,13 @@
+import Button from '../button/Button';
+import './StartScreen.css';
+
+function StartScreen() {
+  return (
+    <div className="start-screen-container">
+      <h1>Le Dinossaur Game</h1>
+      <Button text="Start Game" onClick={() => {}} />
+    </div>
+  );
+}
+
+export default StartScreen;
